9b546ddaabdc203636749362899aa2359d61ea29
Julia Ricketti <jricketti@launchfort.com>
2021-06-13 17:29:04 -0400

    Setup cors

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const fetch = require('node-fetch')
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const port = 3001
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    const { CORS_WHITELIST, NODE_ENV } = process.env
+    const isDev = NODE_ENV === 'development'
+    const whiteList = CORS_WHITELIST.split(',')
+    if (!origin ? isDev : whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/exchangerate', async (req, res, next) => {
+app.get('/exchangerate', cors(corsOptions), async (req, res, next) => {
   const { EXCHANGE_RATE_API_KEY } = process.env
   const { base } = req.query
   try {
